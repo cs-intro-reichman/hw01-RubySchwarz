0c7b5c070950b0b5bebafc495da4c14d5284d16c
Ruby Schvartz <ruby.schvartz@runi.ac.il>
2023-12-25 15:48:48 +0200

    Add awesome code for Homework 1

--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,22 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
-public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+import java.lang.Math;
+
+class GenThree {
+    public static void main(String args[]) {
+
+        int minRange = Integer.parseInt(args[0]);
+        int maxRange = Integer.parseInt(args[1]);
+
+        int num1 = (int) (minRange + Math.random() * (maxRange - minRange));
+        System.out.println(num1);
+
+        int num2 = (int) (minRange + Math.random() * (maxRange - minRange));
+        System.out.println(num2);
+
+        int num3 = (int) (minRange + Math.random() * (maxRange - minRange));
+        System.out.println(num3);
+
+        int minNum = Math.min(num1, Math.min(num2, num3));
+
+        System.out.println("The minimal generated number was " + minNum);
+    }
 }
